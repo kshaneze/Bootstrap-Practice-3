e3de37310c538a8d299737ea94a9565d86c4fcff
niksa <niksaopfor@gmail.com>
2022-04-26 09:12:17 +0200

    Working on project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
 import '../node_modules/bootstrap/js';
+import { Fancybox } from "../node_modules/@fancyapps/ui/src/Fancybox/Fancybox";
+import "node_modules/@fancyapps/ui/dist/fancybox.css";
+      
